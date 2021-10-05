e78d080d5f05a8137acfd402dafb522defbd1102
LucasIluminado <lucask72002@gmail.com>
2021-10-05 14:20:37 -0300

    Adicionando exercicio 1 de Laços de Repetição

--- Exercicios-Eclipse-Java/LaçosdeRepetiçãoEXE1.java
+++ Exercicios-Eclipse-Java/LaçosdeRepetiçãoEXE1.java
@@ -1,10 +1,10 @@
 
-public class LaçosdeReptiçãoEXE1 {
+public class LaçosdeRepetiçãoEXE1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      
-		int num;
+		int num;//numeros para calculo
 		
 		for(num=1000;num<2000; num++)
 		{
