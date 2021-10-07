0798f57b48bc260d2f8a44ad682896825f053b54
LucasIluminado <lucask72002@gmail.com>
2021-10-07 16:03:02 -0300

    Correções

--- Exercicios-Java-POO/TesteAvião.java
+++ Exercicios-Java-POO/TesteAvião.java
@@ -8,7 +8,12 @@ public class TesteAvião {
 		
 		aviao1.imprimir();
 		System.out.println("\n--------------------------------------------------------------------------------------------------");
-		System.out.println("\nBoeing 747 é uma aeronave a jato usada no âmbito civil e militar para transporte de passageiros \ne de carga, referida com frequência como Jumbo Jet ou Queen of the Skies (Rainha dos Céus).\nA sua corcunda na parte superior frontal da fuselagem faz com que seja uma das aeronaves mais \nreconhecíveis do mundo,[3] sendo também a primeira do género produzida em massa. \nFabricada pela Boeing nos Estados Unidos, a versão original do 747 tinha duas vezes e meia mais \ncapacidade de passageiros que o Boeing 707,[4] um dos grandes aviões comerciais dos anos 60. \nEfectuando o seu primeiro voo comercial em 1970, o 747 ostentou o recorde de capacidade de passageiros durante 37 anos.");
+		System.out.println("\nBoeing 747 é uma aeronave a jato usada no âmbito civil e militar para transporte de passageiros "
+				+ "\ne de carga, referida com frequência como Jumbo Jet ou Queen of the Skies (Rainha dos Céus)."
+				+ "\nA sua corcunda na parte superior frontal da fuselagem faz com que seja uma das aeronaves mais "
+				+ "\nreconhecíveis do mundo, sendo também a primeira do género produzida em massa. \nFabricada pela Boeing nos Estados Unidos, a versão original do 747 tinha duas vezes e meia mais "
+				+ "\ncapacidade de passageiros que o Boeing 707, um dos grandes aviões comerciais dos anos 60. "
+				+ "\nEfectuando o seu primeiro voo comercial em 1970, o 747 ostentou o recorde de capacidade de passageiros durante 37 anos.");
 		System.out.println("\n--------------------------------------------------------------------------------------------------");
 	}
 
--- Exercicios-Java-POO/TesteCliente.java
+++ Exercicios-Java-POO/TesteCliente.java
@@ -7,10 +7,11 @@ public class TesteCliente {
 		 Cliente cliente1 = new Cliente ("Lucas B. de Sousa","PADRÃO",25);
 		 
 		 cliente1.imprimir();
-		 System.out.println("\t---------------------------------");
+		 System.out.println("\n\t---------------------------------");
 		 System.out.println("\t\tMUDANÇA DE PLANO!!!");
 		 System.out.println("\t---------------------------------");
 		 cliente1.setPlano("PREMIUM");
+		 cliente1.setValor(35);
 		 cliente1.imprimir();
 	}
 
